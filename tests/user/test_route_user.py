eed76906272bb62c334e186d64620cbf2cff2c6f
Miguel Paricahua Lopez <mig.parlop@gmail.com>
2023-11-17 18:23:54 -0500

    test route uses updated

--- tests/user/test_route_user.py
+++ tests/user/test_route_user.py
@@ -1,9 +1,21 @@
 from fastapi.testclient import TestClient
 from app.main import app
+from unittest.mock import MagicMock, patch
 
 client = TestClient(app)
 
 
 def test_get_users_validate_route():
-    response = client.get("/apiv1/users")
+    mock_service = MagicMock()
+    mock_user1 = MagicMock()
+    mock_user2 = MagicMock()
+    mock_user1.configure_mock(id="1", name="John Doe", email="john@example.com", password="secret")
+    mock_user2.configure_mock(
+        id="2", name="Jane Doe", email="jane@example.com", password="topsecret"
+    )
+    mock_service.get.return_value = [mock_user1, mock_user2]
+
+    with patch("app.user.route_user.user_service", mock_service):
+        response = client.get("/apiv1/users/")
+
     assert response.status_code == 200
